9c39450c083c2528338abab0086687bb1206b3d0
Marcos Paliari <marcos@paliari.com.br>
2017-04-20 19:40:19 -0300

    clear

--- src/v-toaster.js
+++ src/v-toaster.js
@@ -1,13 +1,7 @@
 const Toaster = require('./Toaster.vue')
-// import Toaster from './Toaster'
+
 Toaster.install = (Vue, options) => {
   Vue.prototype.$toaster = new (Vue.extend(Toaster))({propsData: options})
   if (process.env.NODE_ENV === 'development') window.$toaster = Vue.prototype.$toaster
 }
 module.exports = Toaster
-// export default {
-//   install (Vue, options) {
-//     Vue.prototype.$toaster = new (Vue.extend(Toaster))({propsData: options})
-//     if (process.env.NODE_ENV === 'development') window.$toaster = Vue.prototype.$toaster
-//   }
-// }
