e7e8c0a705a0f46993e91950b318847f4ca6a7ed
Marcos Paliari <marcos@paliari.com.br>
2017-04-24 18:28:05 -0300

    identar

--- src/Toaster.vue
+++ src/Toaster.vue
@@ -17,9 +17,9 @@ export default {
   },
   methods: {
     success (message, option = {}) { this.add(message, {theme: 'v-toast-success', timeout: option.timeout}) },
-    info (message, option = {}) { this.add(message, {theme: 'v-toast-info', timeout: option.timeout}) },
+    info    (message, option = {}) { this.add(message, {theme: 'v-toast-info',    timeout: option.timeout}) },
     warning (message, option = {}) { this.add(message, {theme: 'v-toast-warning', timeout: option.timeout}) },
-    error (message, option = {}) { this.add(message, {theme: 'v-toast-error', timeout: option.timeout}) },
+    error   (message, option = {}) { this.add(message, {theme: 'v-toast-error',   timeout: option.timeout}) },
 
     add (message, {theme, timeout}) {
       if (!this.$parent) {
