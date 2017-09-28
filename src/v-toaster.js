4339b495a39877c5f831e2ef771527a3e5ef8a3c
Marcos Paliari <marcos@paliari.com.br>
2017-09-28 19:41:18 -0300

    add Vue.toaster

--- src/v-toaster.js
+++ src/v-toaster.js
@@ -2,6 +2,7 @@ const Toaster = require('./Toaster.vue')
 
 Toaster.install = (Vue, options) => {
   Vue.prototype.$toaster = new (Vue.extend(Toaster))({propsData: options})
+  Vue.toaster = Vue.prototype.$toaster
   if (process.env.NODE_ENV === 'development') window.$toaster = Vue.prototype.$toaster
 }
 module.exports = Toaster
